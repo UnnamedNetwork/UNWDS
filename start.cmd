1ec6139cbe728c945f3f4209dc7502f052ccc223
Cuong Tien Dinh <34135258+CuongZ@users.noreply.github.com>
2019-11-22 21:17:38 +0700

    edit server loader script.

--- start.cmd
+++ start.cmd
@@ -1,7 +1,21 @@
 @echo off
-TITLE UNWDelicatedSoftware for Minecraft: Bedrock Edition
+TITLE UNWDelicatedSoftware software for Minecraft: Bedrock Edition
 cd /d %~dp0
-
+echo ==========UNWDelicatedSoftware software load==========
+echo = - Server will start...
+echo = - Checking Server file...
+if exist UNWDelicatedSoftware.phar (
+	set UNWDS_FILE=UNWDelicatedSoftware.phar
+	echo = - Found
+	echo = - Server starting...
+	echo ==================================================
+) else (
+	echo = - UNWDelicatedSoftware.phar not found
+	echo = - Server software can be found at github.com/CuongZ/UNWDelicatedSoftware/releases
+    echo ==================================================
+	pause
+	exit 1
+)
 if exist bin\php\php.exe (
 	set PHPRC=""
 	set PHP_BINARY=bin\php\php.exe
@@ -9,22 +23,9 @@ if exist bin\php\php.exe (
 	set PHP_BINARY=php
 )
 
-if exist PocketMine-MP.phar (
-	set SOFTWARE_FILE=PocketMine-MP.phar
-) else if exist UNWDelicatedSoftware.phar (
-    set SOFTWARE_FILE=UNWDelicatedSoftware.phar
-) else if exist src\pocketmine\PocketMine.php (
-    set SOFTWARE_FILE=start.php
-) else (
-	echo UNWDelicatedSoftware.phar not found
-	echo Downloads can be found at github.com/CuongZ/UNWDelicatedSoftware/releases
-	pause
-	exit 1
-)
-
 if exist bin\mintty.exe (
-	start %PHP_BINARY% %SOFTWARE_FILE% --enable-ansi %*
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "UNWDelicatedSoftware" -i bin/unwicon.ico -w max %PHP_BINARY% %UNWDS_FILE% --enable-ansi %*
 ) else (
-	REM pause on exitcode != 0 so the user can see what went wrong
-	%PHP_BINARY% -c bin\php %SOFTWARE_FILE %* || pause
+	REM
+	%PHP_BINARY% -c bin\php %UNWDS_FILE% %* || pause
 )
