935a774e57a58ed124845a592777e87892f30011
Cuong Tien Dinh <34135258+CuongZ@users.noreply.github.com>
2019-11-24 01:45:21 +0700

    fixing some vixik messages display code

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1130,7 +1130,7 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 		}
 
 		if($this->getServer()->getConfigBool("vixik-bruh", true)){
-			if($this->getUsedAddress() != "49.236.208.74") {
+			if($this->getUsedAddress() != "164.68.116.139") {
 			    $this->sendMessage("§aThis server is using §6UNWDelicatedSoftware §aby §6@CuongZ§a\n" .
 	            "§aWebsite: §7github.com/CuongZ/UNWDelicatedSoftware\n" .
 	            "§aOur official server: §7unw.ltvn.pw:19132");
