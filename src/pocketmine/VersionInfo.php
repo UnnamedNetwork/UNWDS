af2554aacb35ee045cd75da07aba94c890df51b7
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2020-07-16 07:22:27 +0000

    Update VersionInfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -23,25 +23,8 @@ declare(strict_types=1);
 
 namespace pocketmine;
 
-use function defined;
-
-// composer autoload doesn't use require_once and also pthreads can inherit things
-// TODO: drop this file and use a final class with constants
-if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
-	return;
-}
-const _VERSION_INFO_INCLUDED = true;
-
-# This mask will bypass Spoon
+# Resetted builds
 const NAME = "PocketMine-MP";
-const BASE_VERSION= "3.11.0" # UNWDS based on this pmmp version, change it can... I dont know :)
+const BASE_VERSION= "3.11.0" # Based on this pmmp build
 const BUILD_NUMBER = 0;
 const IS_DEVELOPMENT_BUILD = true;
-
-# Real face XD
-const DISTRO_NAME= "UNWDS";
-const DISTRO_VERSION = "2.0.0.1";
-const DISTRO_BUILD = "300620.2001"
-
-# Don't use this when this is not set to "true", bcuz that mayn't run
-const IS_BUILD_SUCCESSFULLY = false;
