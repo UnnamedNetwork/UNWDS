433bd64116a21ee14624678c89877c6d748ac81f
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2020-11-01 21:33:16 +0700

    Update VersionCommand.php

--- src/pocketmine/command/defaults/VersionCommand.php
+++ src/pocketmine/command/defaults/VersionCommand.php
@@ -53,6 +53,7 @@ class VersionCommand extends VanillaCommand{
 		if(count($args) === 0){
 			$sender->sendMessage(new TranslationContainer("pocketmine.server.info.extended", [
 				$sender->getServer()->getName(),
+			        $sender->getServer()->getCodename(),
 				$sender->getServer()->getPocketMineVersion(),
 				$sender->getServer()->getVersion(),
 				ProtocolInfo::CURRENT_PROTOCOL
