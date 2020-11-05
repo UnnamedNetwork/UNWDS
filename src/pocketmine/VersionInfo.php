692a29fef50df7a058c20873c862cff693cf51fb
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2020-11-05 19:51:49 +0700

    and putting up all again.

---
+++ src/pocketmine/VersionInfo.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine;
+
+use function defined;
+
+// composer autoload doesn't use require_once and also pthreads can inherit things
+// TODO: drop this file and use a final class with constants
+if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
+	return;
+}
+const _VERSION_INFO_INCLUDED = true;
+
+const NAME = "UNWDS";
+const BASE_VERSION = "2.0.2";
+const IS_DEVELOPMENT_BUILD = true;
+const BUILD_NUMBER = 0011;
+const CODENAME = "Reborn";
+
+// these should just appear here... just giving some info for debugging in our special plugin after build into phar file.
+const DEV_BUILDVENDOR = "UNWDS DevTools v1.2";
+const DEV_BUILDINFO = "dtcu0ng@01.11.2020";
+
