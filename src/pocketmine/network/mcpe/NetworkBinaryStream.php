87f5902345df478a8a95d218205fa822ba44f19f
dtcu0ng <deptteam.cuong@gmail.com>
2020-11-22 11:04:40 +0700

    Fixed missing field on Persona skin encode

--- src/pocketmine/network/mcpe/NetworkBinaryStream.php
+++ src/pocketmine/network/mcpe/NetworkBinaryStream.php
@@ -148,6 +148,7 @@ class NetworkBinaryStream extends BinaryStream{
 			$this->putSkinImage($animation->getImage());
 			($this->buffer .= (\pack("V", $animation->getType())));
 			($this->buffer .= (\pack("g", $animation->getFrames())));
+			$this->putLInt($animation->getExpressionType());
 		}
 		$this->putSkinImage($skin->getCapeImage());
 		$this->putString($skin->getGeometryData());
