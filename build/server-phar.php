62143ccec0cad64542cc1b8184293ef4548f120a
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2020-11-22 22:00:59 +0700

    Update server-phar.php

--- build/server-phar.php
+++ build/server-phar.php
@@ -23,7 +23,6 @@ declare(strict_types=1);
 
 namespace pocketmine\build\server_phar;
 
-use pocketmine\utils\Git;
 use function array_map;
 use function count;
 use function defined;
@@ -135,13 +134,6 @@ function main() : void{
 		exit(1);
 	}
 
-	$opts = getopt("", ["out:", "git:"]);
-	if(isset($opts["git"])){
-		$gitHash = $opts["git"];
-	}else{
-		$gitHash = Git::getRepositoryStatePretty(dirname(__DIR__));
-		echo "Git hash detected as $gitHash" . PHP_EOL;
-	}
 	foreach(buildPhar(
 		$opts["out"] ?? getcwd() . DIRECTORY_SEPARATOR . "PocketMine-MP.phar",
 		dirname(__DIR__) . DIRECTORY_SEPARATOR,
@@ -150,7 +142,6 @@ function main() : void{
 			'vendor'
 		],
 		[
-			'git' => $gitHash
 		],
 		<<<'STUB'
 <?php
