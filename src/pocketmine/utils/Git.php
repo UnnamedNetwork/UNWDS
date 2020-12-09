d8ca6f3af6d8c1ce78b76164231a315981dcd910
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2020-12-09 11:16:57 +0700

    1.16.200 protocol update
    merged https://github.com/pmmp/PocketMine-MP/commit/692e63ad7cf2c97a3a85f60fc45f7816593b3cbe into UNWDS (stable)

--- src/pocketmine/utils/Git.php
+++ src/pocketmine/utils/Git.php
@@ -1,3 +1,4 @@
 <?php
+
 // I don't know how this file are needed to run the server...
-// But I don't want to touch into PocketMine-MP vendor, so this will be empty and being useless now.
+// But I don't want to touch into PocketMine-MP vendor, so this will be empty and being useless now
