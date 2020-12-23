01d0ff1d9b866e181b84be59a2c07b2f44d0f6e7
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2020-12-24 02:25:28 +0700

    release 2.2.1

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -36,4 +36,4 @@ const _VERSION_INFO_INCLUDED = true;
   // PocketMine-MP's default version constants, this will bypass the Spoon detector
 const NAME = "PocketMine-MP";
 const BASE_VERSION = "3.17.1";
-const IS_DEVELOPMENT_BUILD = true;
+const IS_DEVELOPMENT_BUILD = false;
