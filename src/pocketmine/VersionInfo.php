61e400232b661b958ac2fc2509c536fee9d72571
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2021-04-06 11:47:47 +0700

    codestyle check

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -27,16 +27,15 @@ use function defined;
 
 // composer autoload doesn't use require_once and also pthreads can inherit things
 // TODO: drop this file and use a final class with constants
-// we deleted SpoonMask.php because we just need only 1 file that contain all version info...
 if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 	return;
 }
 const _VERSION_INFO_INCLUDED = true;
-// Name section
-const NAME = "PocketMine-MP"; // This name will bypass the SpoonDetector and plugins.
-const DISTRO_NAME = "UNWDS"; // The name will appear at CLI, /version command, etc... FAKE NAME!
+
+const NAME = "PocketMine-MP";
+const DISTRO_NAME = "UNWDS";
 const CODENAME = "Twee";
-const BASE_VERSION = "3.18.2"; // Real API version, plugins need corresponding PocketMine-MP base version as API version to run, so we not recommend to change this. 
-const DISTRO_VERSION = "2.3.2"; // Like distro name but it's version
+const BASE_VERSION = "3.18.2";
+const DISTRO_VERSION = "2.3.2";
 const IS_DEVELOPMENT_BUILD = true;
 const BUILD_NUMBER = 0333;
