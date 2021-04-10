fc6d0fd99af5e9e9f89740df000b014d0c11b2ec
Cường Tiến Đinh <deptteam.cuong@gmail.com>
2021-04-10 23:25:11 +0700

    removed some lines since we don't use Travis anymore

--- tests/push_output.sh
+++ tests/push_output.sh
@@ -15,7 +15,6 @@ PLUGINS_DIR="$DATA_DIR/plugins"
 
 dateAndMonth=`date`
 BUILDPHPV=$(php -r 'echo PHP_VERSION;')
-NBPHPV="7.3.27"
 OLDBLD=$(expr $GITHUB_RUN_NUMBER - 1)
 OUTPUT_REPO="UNWDS_Output/branch"
 
@@ -38,7 +37,6 @@ fi
 
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
-echo "PHP $BUILDPHPV detected. Pushing the phar into output repo..."
 chmod 777 UNWDS.phar
 git clone https://github.com/dtcu0ng/UNWDS_Output.git
 cd UNWDS_Output
@@ -54,11 +52,10 @@ cd ../../
 cp UNWDS.phar $OUTPUT_REPO/${GITHUB_REF##*/}/latest
 cd UNWDS_Output
 git add -A
-git commit -m "${GITHUB_REF##*/} build: $dateAndMonth (CI #$GITHUB_RUN_NUMBER)"
+git commit -m "Build from ${GITHUB_REF##*/}: $dateAndMonth (CI #$GITHUB_RUN_NUMBER)"
 git remote rm origin
 # Add new "origin" with access token in the git URL for authentication
 git remote add origin https://dtcu0ng:$GHTOKEN@github.com/dtcu0ng/UNWDS_Output.git > /dev/null 2>&1
 git pull origin master --rebase
 git push origin master --quiet
-echo Push completed with 0 or more errors
 echo Branch: ${GITHUB_REF##*/}
