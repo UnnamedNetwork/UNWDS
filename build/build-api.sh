1d2e311be17a3532fa9c524754cd0e73e76b6aa3
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2021-08-07 03:25:26 +0700

    okay now its work

--- build/build-api.sh
+++ build/build-api.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install jo
-
 #prepare variable we need.
 BUILD_TOKEN="$GHTOKEN"
 ApiRepoUrl="https://github.com/UnnamedNetwork/unnamednetwork.github.io"
@@ -10,13 +8,15 @@ Org="UnnamedNetwork"
 DistroName=$(php -r 'require "vendor/autoload.php"; echo \pocketmine\DISTRO_NAME;')
 PhpVersion="7.4"
 DistroVersion=$(php -r 'require "vendor/autoload.php"; echo \pocketmine\DISTRO_VERSION;')
-CalcedBuildNumber=1000+$GITHUB_RUN_NUMBER
+CalcedBuildNumber=1000+$GITHUB_RUN_ID
 sed -i "s/const BUILD_NUMBER = 0333/const BUILD_NUMBER = ${CalcedBuildNumber}/" src/pocketmine/VersionInfo.php
 BuildNumber=$(php -r 'require "vendor/autoload.php"; echo \pocketmine\BUILD_NUMBER;')
 IsDev=$(php -r 'require "vendor/autoload.php"; echo \pocketmine\IS_DEVELOPMENT_BUILD;')
 Branch="${GITHUB_REF##*/}"
 GitCommit="${GITHUB_SHA}"
 TargetVersion=$(php -r 'require "vendor/autoload.php"; echo \pocketmine\network\mcpe\protocol\ProtocolInfo::MINECRAFT_VERSION_NETWORK;')
+PharName="$DistroName.phar"
+Dummy=""
 Date=$(date +"%s")
 
 rm api.json 2> /dev/null
@@ -30,7 +30,7 @@ git checkout main
 cd $DistroName/version_control/
 
 # the work will here.
-MakeJSON=$(jo -p job=$DistroName php_version=$PhpVersion base_version=$DistroVersion build_number=$BuildNumber is_dev=$IsDev branch=$Branch git_commit=$GitCommit mcpe_version=$TargetVersion phar_name=$PharName dummy= build=$BuildNumber date=$Date details_url=https://github.com/$Org/$DistroName/releases/v$DistroVersion download_url=https://github.com/$Org/$DistroName/releases/downloads/v$DistroVersion/$DistroName.phar)
+MakeJSON=$(jo -p job=$DistroName php_version=$PhpVersion base_version=$DistroVersion build_number=$BuildNumber is_dev=$IsDev branch=$Branch git_commit=$GitCommit mcpe_version=$TargetVersion phar_name=$PharName dummy=$Dummy build=$BuildNumber date=$Date details_url=https://github.com/$Org/$DistroName/releases/v$DistroVersion download_url=https://github.com/$Org/$DistroName/releases/downloads/v$DistroVersion/$DistroName.phar)
 echo "$MakeJSON"
 echo "$MakeJSON" >> api.json
 
