5a309c22b93b597107f962b1759be735141a4af7
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2021-08-13 19:33:13 +0700

    fixing..

--- build/build-api.sh
+++ build/build-api.sh
@@ -52,7 +52,7 @@ function Main {
         DetailsURL="https://github.com/$Org/$DistroName/releases/v$DistroVersion"
         DownloadURL="https://github.com/$Org/$DistroName/releases/download/v$DistroVersion/$DistroName.phar"
 	    BuildJSON
-	elif [ "$Branch" = "stable" || "$Branch" = "master" ]; then
+	elif [[ "$Branch" = "stable" ] || ["$Branch" = "master" ]]; then
         APIFile="api_$Branch.json"
         DetailsURL="https://github.com/$Org/$DistroName/commit/$GitCommit"
         DownloadURL="https://github.com/$Org/build-repo/raw/master/$DistroName/branch/$Branch/$BuildNumber/UNWDS.phar"
