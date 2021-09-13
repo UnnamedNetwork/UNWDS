4afffb5d22e64855105ed3e831e46e79d00b56a0
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2021-09-13 08:52:22 +0700

    2.7.5

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -37,6 +37,6 @@ const DISTRO_NAME = "UNWDS";
 const CODENAME = "Twee";
 const BASE_VERSION = "3.22.5";
 const DISTRO_VERSION = "2.7.5";
-const IS_DEVELOPMENT_BUILD = true;
+const IS_DEVELOPMENT_BUILD = false;
 const BUILD_NUMBER = 0333;
 const BUILD_CHANNEL = "";
